7996d9349a35dd9118ee341d3ae387858a30e735
C4rnivore <kirill.z13@mail.ru>
2024-05-22 12:44:03 +0500

    front fix

--- IllustrationClient/vite.config.ts
+++ IllustrationClient/vite.config.ts
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:3000,
-    strictPort:true,
-    host:true,
-    origin: "http://0.0.0.0:3000"
+    host: true,
+    strictPort: true,
+    port: 3000,
   }
 })
